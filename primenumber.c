03048a629d388b994f91c88259a3cab11344aaed
pramodkallihal2-gif <pramodkallihal2@gmail.com>
2025-11-02 23:08:16 +0530

    Enhance prime number program: Add counter and improve output formatting

--- primenumber.c
+++ primenumber.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
-int main ()
+int main()
 {
-    int n,i,j,count;
-    printf("Enter the value of b :");
+    int n,i,j,p=0;
+    int count;
+    printf("Enter the value of n :");
     scanf("%d",&n);
-    printf("The prime number between 1 and %d are : \n",n);
-    for(i=0;i<=n;i++)
-    {
-        count=0;
-        for(j=1;j<=i;j++)
-        {
-            if(i%j==0)
+    for(i=1;i<=n;i++)
+   {  count=0;
+      for(j=1;j<=i;j++)
+     {   if (i%j==0)
             count++;
-        }
-        if (count==2)
-        {
-            printf("%d \n",i);
-        }
-    }
-    return 0 ;
-    
+     }
+     if(count==2)
+     {printf("%d ,",i);
+     p++;  
+     }
 
-}
+     }
+     printf("\n The total number between 1 to %d is %d.",n,p);
+     return 0;
+     }
+
